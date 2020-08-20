cee36e939defdefaeb569475d5e7b02d03a3257f
Glitch (hello-express) <none>
2020-08-20 01:47:37 +0000

    💍🐏 Checkpoint ./server.js:2079434/20

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const dbURI = process.env.DB_CONNECTION;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
+  .then(result => app.listen(process.env.PORT))
   .catch(err => console.log(err));
 
 // register view engine
